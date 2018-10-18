476d2e2547dd3ee4d1c50ab9e1b33e9ca83f9dcf
Skyla Moore <s.moore5104@gmail.com>
2018-10-18 11:31:12 +0100

    Finished file, successful outputs

--- ex11.py
+++ ex11.py
@@ -5,9 +5,15 @@ myInt = randint(1,9)
 def GuessNum():
     userGuess = int(input("I'm thinking of a number between 1 and 9, take a guess: "))
     if(userGuess != myInt):
-        GuessNum()
+        if(userGuess < myInt):
+            print("Aim higher!")
+            GuessNum()
+        elif(userGuess > myInt):
+            print("Aim lower!")
+            GuessNum()
+        else:
+            GuessNum()
     else:
         print("You got it! my number was", myInt, "And you guessed", userGuess)
 GuessNum()
         
-
